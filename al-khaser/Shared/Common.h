fe16cf8b02ab0ce7d319f9628e4c9c916ef45068
Lord Noteworthy <165647@supinfo.com>
2015-11-12 21:46:53 +0100

    Add Commun functions

---
+++ al-khaser/Shared/Common.h
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+VOID print_detected() ;
+VOID print_not_detected() ;
+VOID print_category(TCHAR* text);
+VOID print_last_error(LPTSTR lpszFunction);
+VOID exec_check(int(*callback)(), TCHAR* text_log);
+VOID print_os();
