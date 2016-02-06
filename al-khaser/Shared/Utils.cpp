c1780029e882d85d355612cf86b4815f229fc5ce
Lord Noteworthy <lordnoteworthy@protonmail.com>
2016-02-06 19:20:52 +0100

    update code

--- al-khaser/Shared/Utils.cpp
+++ al-khaser/Shared/Utils.cpp
@@ -581,3 +581,91 @@ DWORD GetMainThreadId(DWORD pid)
 	return (DWORD)0;
 }
 
+
+DWORD QueryWMI()
+{
+	// Initialize COM.
+	HRESULT hres;
+	hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+	if (FAILED(hres)) {
+		print_last_error(_T("CoInitializeEx"));
+		return 0;
+	}
+
+	// Set general COM security levels
+	hres = CoInitializeSecurity(
+		NULL,
+		-1,                          // COM authentication
+		NULL,                        // Authentication services
+		NULL,                        // Reserved
+		RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+		NULL,                        // Authentication info
+		EOAC_NONE,                   // Additional capabilities 
+		NULL                         // Reserved
+		);
+
+	if (FAILED(hres)) {
+		print_last_error(_T("CoInitializeSecurity"));
+		CoUninitialize();
+		return 0;
+	}
+
+	// Obtain the initial locator to WMI 
+	IWbemLocator *pLoc = NULL;
+	hres = CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID *)&pLoc);
+	if (FAILED(hres)) {
+		print_last_error(_T("CoCreateInstance"));
+		CoUninitialize();
+		return 0;
+	}
+
+	// Connect to WMI through the IWbemLocator::ConnectServer method
+	IWbemServices *pSvc = NULL;
+	// Connect to the root\cimv2 namespace with
+	// the current user and obtain pointer pSvc
+	// to make IWbemServices calls.
+	hres = pLoc->ConnectServer(
+		BSTR(L"ROOT\\CIMV2"),	 // Object path of WMI namespace
+		NULL,                    // User name. NULL = current user
+		NULL,                    // User password. NULL = current
+		0,                       // Locale. NULL indicates current
+		NULL,                    // Security flags.
+		0,                       // Authority (for example, Kerberos)
+		0,                       // Context object 
+		&pSvc                    // pointer to IWbemServices proxy
+		);
+
+	if (FAILED(hres)){
+		print_last_error(_T("ConnectServer"));
+		pLoc->Release();
+		CoUninitialize();
+		return 1;
+	}
+
+	cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
+
+
+	// Step 5: --------------------------------------------------
+	// Set security levels on the proxy -------------------------
+
+	hres = CoSetProxyBlanket(
+		pSvc,                        // Indicates the proxy to set
+		RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+		RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+		NULL,                        // Server principal name 
+		RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+		NULL,                        // client identity
+		EOAC_NONE                    // proxy capabilities 
+		);
+
+	if (FAILED(hres))
+	{
+		cout << "Could not set proxy blanket. Error code = 0x"
+			<< hex << hres << endl;
+		pSvc->Release();
+		pLoc->Release();
+		CoUninitialize();
+		return 1;               // Program has failed.
+	}
