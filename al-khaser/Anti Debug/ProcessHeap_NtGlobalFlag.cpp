f339953be5b76410381704ed087298818f745693
mrexodia <mr.exodia.tpodt@gmail.com>
2016-06-28 04:36:25 +0200

    fixed NtGlobalFlag (incorrect assumptions about the position of the WOW64 PEB)

--- al-khaser/Anti Debug/ProcessHeap_NtGlobalFlag.cpp
+++ al-khaser/Anti Debug/ProcessHeap_NtGlobalFlag.cpp
@@ -10,24 +10,36 @@ are set for the process, and we can use this to our advantage to identify if our
 			FLG_HEAP_VALIDATE_PARAMETERS
 */
 
-BOOL NtGlobalFlag ()
+BOOL NtGlobalFlag()
 {
-	PDWORD pNtGlobalFlag, pNtGlobalFlagWoW64 = NULL;
+	PDWORD pNtGlobalFlag = NULL, pNtGlobalFlagWoW64 = NULL;
 
 #if defined (ENV64BIT)
+	BYTE* _teb64 = (BYTE*)__readgsqword(0x30);
+	DWORD64 _peb64 = *(DWORD64*)(_teb64 + 0x60);
 	pNtGlobalFlag = (PDWORD)(__readgsqword(0x60) + 0xBC);
 
 #elif defined(ENV32BIT)
 	/* NtGlobalFlags for real 32-bits OS */
-	pNtGlobalFlag = (PDWORD)(__readfsdword(0x30) + 0x68);
+	BYTE* _teb32 = (BYTE*)__readfsdword(0x18);
+	DWORD _peb32 = *(DWORD*)(_teb32 + 0x30);
+	pNtGlobalFlag = (PDWORD)(_peb32 + 0x68);
 
 	if (IsWoW64())
+	{
 		/* In Wow64, there is a separate PEB for the 32-bit portion and the 64-bit portion
 		which we can double-check */
-		pNtGlobalFlagWoW64 = (PDWORD)(__readfsdword(0x30) + 0x10bc);
+		
+		BYTE* _teb64 = (BYTE*)__readfsdword(0x18) - 0x2000;
+		DWORD64 _peb64 = *(DWORD64*)(_teb64 + 0x60);
+		pNtGlobalFlagWoW64 = (PDWORD)(_peb64 + 0xBC);
+	}
 #endif
 
-	if (*pNtGlobalFlag & 0x00000070 || (pNtGlobalFlagWoW64 && *pNtGlobalFlagWoW64 & 0x00000070))
+	bool normalDetected = pNtGlobalFlag && *pNtGlobalFlag & 0x00000070;
+	bool wow64Detected = pNtGlobalFlagWoW64 && *pNtGlobalFlagWoW64 & 0x00000070;
+	
+	if(normalDetected || wow64Detected)
 		return TRUE;
 	else
 		return FALSE;
