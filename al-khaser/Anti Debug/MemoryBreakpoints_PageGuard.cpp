7ba77a173e528470f73b6bd25fc9374c04b7dbf4
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2016-07-01 11:43:53 +0200

    x64 PAGE_GUARD working

--- al-khaser/Anti Debug/MemoryBreakpoints_PageGuard.cpp
+++ al-khaser/Anti Debug/MemoryBreakpoints_PageGuard.cpp
@@ -30,25 +30,15 @@ BOOL MemoryBreakpoints_PageGuard()
 	if (VirtualProtect(pAllocation, SystemInfo.dwPageSize, PAGE_EXECUTE_READWRITE | PAGE_GUARD, &OldProtect) == 0)
 		return FALSE;
 
-// I tested it only in the 32bits version of Olly since the 64bits is not their yet atm.
-#if defined (ENV32BIT)
 	__try
 	{
-		__asm mov eax, pAllocation
-		// This is the address we'll return to if we're under a debugger
-		__asm push MemBpBeingDebugged
-		__asm jmp eax // Exception or execution, which shall it be :D?
-	
+		((void(*)())pAllocation)(); // Exception or execution, which shall it be :D?
 	}
-	__except (EXCEPTION_EXECUTE_HANDLER)
+	__except (GetExceptionCode() == STATUS_GUARD_PAGE_VIOLATION ? EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_SEARCH)
 	{
-		// The exception occured and no debugger was detected
 		VirtualFree(pAllocation, NULL, MEM_RELEASE);
 		return FALSE;
 	}
-
-	__asm MemBpBeingDebugged:
-#endif
 
 	VirtualFree(pAllocation, NULL, MEM_RELEASE);
 	return TRUE;
