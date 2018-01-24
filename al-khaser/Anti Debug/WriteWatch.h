51685610f54187f818fcc71209707214aff7b132
Graham Sutherland <gsuberland@gmail.com>
2018-01-24 22:46:06 +0000

    Add write watch debugger detection
    Detects external memory accesses by debuggers and hooks using the
    MEM_WRITE_WATCH feature of VirtualAlloc, using 4 different approaches.

---
+++ al-khaser/Anti Debug/WriteWatch.h
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+
+BOOL VirtualAlloc_WriteWatch_BufferOnly();
+BOOL VirtualAlloc_WriteWatch_APICall();
+BOOL VirtualAlloc_WriteWatch_IsDebuggerPresent();
+BOOL VirtualAlloc_WriteWatch_CodeWrite();
