25a75a7d825c90222777d28d2472e2253ca5658e
hfiref0x <hfiref0x@users.noreply.github.com>
2019-01-20 13:55:06 +0700

    Fix multiple bugs in ProcessJob routine
    Fix invalid handle value passed to CloseHandle, number of null pointer dereferences. Fix typo in main exec_check(&ProcessJob ...).

--- al-khaser/AntiDebug/ProcessJob.cpp
+++ al-khaser/AntiDebug/ProcessJob.cpp
@@ -20,54 +20,59 @@ BOOL ProcessJob()
 	DWORD jobProcessStructSize = sizeof(JOBOBJECT_BASIC_PROCESS_ID_LIST) + sizeof(ULONG_PTR) * 1024;
 	JOBOBJECT_BASIC_PROCESS_ID_LIST* jobProcessIdList = static_cast<JOBOBJECT_BASIC_PROCESS_ID_LIST*>(malloc(jobProcessStructSize));
 
-	SecureZeroMemory(jobProcessIdList, jobProcessStructSize);
+	if (jobProcessIdList) {
 
-	jobProcessIdList->NumberOfProcessIdsInList = 1024;
+		SecureZeroMemory(jobProcessIdList, jobProcessStructSize);
 
-	if (QueryInformationJobObject(NULL, JobObjectBasicProcessIdList, jobProcessIdList, jobProcessStructSize, NULL) == TRUE)
-	{
-		int ok_processes = 0;
-		for (DWORD i = 0; i < jobProcessIdList->NumberOfAssignedProcesses; i++)
+		jobProcessIdList->NumberOfProcessIdsInList = 1024;
+
+		if (QueryInformationJobObject(NULL, JobObjectBasicProcessIdList, jobProcessIdList, jobProcessStructSize, NULL) == TRUE)
 		{
-			ULONG_PTR processId = jobProcessIdList->ProcessIdList[i];
+			int ok_processes = 0;
+			for (DWORD i = 0; i < jobProcessIdList->NumberOfAssignedProcesses; i++)
+			{
+				ULONG_PTR processId = jobProcessIdList->ProcessIdList[i];
 
-			// is this the current process? if so that's ok
-			if (processId == (ULONG_PTR)GetCurrentProcessId())
-			{
-				ok_processes++;
-			}
-			else
-			{
-				// find the process name for this job process
-				HANDLE hJobProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, processId);
-				if (hJobProcess != NULL)
+				// is this the current process? if so that's ok
+				if (processId == (ULONG_PTR)GetCurrentProcessId())
 				{
-					const int processNameBufferSize = 4096;
-					LPTSTR processName = static_cast<LPTSTR>(malloc(sizeof(TCHAR) * processNameBufferSize));
-					SecureZeroMemory(processName, sizeof(TCHAR) * processNameBufferSize);
-
-					if (GetProcessImageFileName(hJobProcess, processName, processNameBufferSize) > 0)
-					{
-						String pnStr(processName);
-
-						// ignore conhost.exe (this hosts the al-khaser executable in a console)
-						if (pnStr.find(String(L"\\Windows\\System32\\conhost.exe")) != std::string::npos)
-						{
-							ok_processes++;
-						}
-					}
-
-					free(processName);
+					ok_processes++;
 				}
-				CloseHandle(hJobProcess);
+				else
+				{
+
+					// find the process name for this job process
+					HANDLE hJobProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, (DWORD)processId);
+					if (hJobProcess != NULL)
+					{
+						const int processNameBufferSize = 4096;
+						LPTSTR processName = static_cast<LPTSTR>(malloc(sizeof(TCHAR) * processNameBufferSize));
+						if (processName) {
+							SecureZeroMemory(processName, sizeof(TCHAR) * processNameBufferSize);
+
+							if (GetProcessImageFileName(hJobProcess, processName, processNameBufferSize) > 0)
+							{
+								String pnStr(processName);
+
+								// ignore conhost.exe (this hosts the al-khaser executable in a console)
+								if (pnStr.find(String(L"\\Windows\\System32\\conhost.exe")) != std::string::npos)
+								{
+									ok_processes++;
+								}
+							}
+
+							free(processName);
+						}
+						CloseHandle(hJobProcess);
+					}
+				}
 			}
+
+			// if we found other processes in the job other than the current process and conhost, report a problem
+			foundProblem = ok_processes != jobProcessIdList->NumberOfAssignedProcesses;
 		}
 
-		// if we found other processes in the job other than the current process and conhost, report a problem
-		foundProblem = ok_processes != jobProcessIdList->NumberOfAssignedProcesses;
+		free(jobProcessIdList);
 	}
-
-	free(jobProcessIdList);
-
 	return foundProblem;
 }
