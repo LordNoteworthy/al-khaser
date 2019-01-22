f0faf5a982a13652ebfcbb17dc8bd3401a46180b
hfiref0x <hfiref0x@users.noreply.github.com>
2019-01-22 21:24:52 +0700

    Fix for HardwareBreakpoints routine
    Fix null pointer dereference, fix memory leak.

--- al-khaser/AntiDebug/HardwareBreakpoints.cpp
+++ al-khaser/AntiDebug/HardwareBreakpoints.cpp
@@ -11,23 +11,31 @@ Dr0 through Dr3 are 32 bit registers that hold the address of the breakpoint .
 
 BOOL HardwareBreakpoints()
 {
+	BOOL bResult = FALSE;
+
 	// This structure is key to the function and is the 
 	// medium for detection and removal
 	PCONTEXT ctx = PCONTEXT(VirtualAlloc(NULL, sizeof(CONTEXT), MEM_COMMIT, PAGE_READWRITE));
-	SecureZeroMemory(ctx, sizeof(CONTEXT));
 
-	// The CONTEXT structure is an in/out parameter therefore we have
-	// to set the flags so Get/SetThreadContext knows what to set or get.
-	ctx->ContextFlags = CONTEXT_DEBUG_REGISTERS;
+	if (ctx) {
 
-	// Get the registers
-	if (GetThreadContext(GetCurrentThread(), ctx) == 0)
-		return -1;
+		SecureZeroMemory(ctx, sizeof(CONTEXT));
 
-	// Now we can check for hardware breakpoints, its not 
-	// necessary to check Dr6 and Dr7, however feel free to
-	if (ctx->Dr0 != 0 || ctx->Dr1 != 0 || ctx->Dr2 != 0 || ctx->Dr3 != 0)
-		return TRUE;
-	else
-		return FALSE;
+		// The CONTEXT structure is an in/out parameter therefore we have
+		// to set the flags so Get/SetThreadContext knows what to set or get.
+		ctx->ContextFlags = CONTEXT_DEBUG_REGISTERS;
+
+		// Get the registers
+		if (GetThreadContext(GetCurrentThread(), ctx)) {
+
+			// Now we can check for hardware breakpoints, its not 
+			// necessary to check Dr6 and Dr7, however feel free to
+			if (ctx->Dr0 != 0 || ctx->Dr1 != 0 || ctx->Dr2 != 0 || ctx->Dr3 != 0)
+				bResult = TRUE;
+		}
+
+		VirtualFree(ctx, 0, MEM_RELEASE);
+	}
+
+	return bResult;
 }
