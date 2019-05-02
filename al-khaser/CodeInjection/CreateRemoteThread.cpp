ae326e0004dc19ac84c25b10aff69df8e40452d3
lurumdare <notvalidemail213123@brain.xyz>
2019-05-02 10:39:46 +0300

    need review for refactoring CreateRemoteThread

--- al-khaser/CodeInjection/CreateRemoteThread.cpp
+++ al-khaser/CodeInjection/CreateRemoteThread.cpp
@@ -4,49 +4,61 @@
 
 BOOL CreateRemoteThread_Injection()
 {
-	/* Some vars */
-	DWORD dwProcessId;
-	HANDLE hProcess = nullptr, hRemoteThread = nullptr;
-	HMODULE hKernel32;
-	FARPROC LoadLibraryAddress;
 	LPVOID lpBaseAddress = nullptr;
 	TCHAR lpDllName[] = _T("InjectedDLL.dll");
 	TCHAR lpDllPath[MAX_PATH];
-	SIZE_T dwSize;
-	BOOL bStatus = FALSE, bDebugPrivilegeEnabled;
-	
+	auto bStatus = FALSE;
+
 	/* Get Process ID from Process name */
-	dwProcessId = GetProcessIdFromName(_T("notepad.exe"));
+	const auto dwProcessId = GetProcessIdFromName(_T("notepad.exe"));
 	if (dwProcessId == NULL)
 		return FALSE;
 	_tprintf(_T("\t[+] Getting proc id: %u\n"), dwProcessId);
 
 	/* Set Debug privilege */
-	bDebugPrivilegeEnabled = SetDebugPrivileges();
+	const auto bDebugPrivilegeEnabled = SetDebugPrivileges();
 	_tprintf(_T("\t[+] Setting Debug Privileges [%d]\n"), bDebugPrivilegeEnabled);
 	if (!bDebugPrivilegeEnabled)
 		return FALSE;
 
 	/* Obtain a handle the process */
-	hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId);
-	if (hProcess == nullptr) {
+	const auto hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId);
+	if (hProcess == nullptr)
+	{
 		print_last_error(_T("OpenProcess"));
-		goto Cleanup;
+		/* If lpBaseAddress initialized then hProcess is initialized too because of upper check. */
+		if (lpBaseAddress)
+		{
+			VirtualFreeEx(hProcess, lpBaseAddress, 0, MEM_RELEASE);
+		}
+		if (hProcess) CloseHandle(hProcess);
 	}
 
 	/* Obtain a handle to kernel32 */
-	hKernel32 = GetModuleHandle(_T("kernel32.dll"));
-	if (hKernel32 == nullptr) {
+	const auto hKernel32 = GetModuleHandle(_T("kernel32.dll"));
+	if (hKernel32 == nullptr)
+	{
 		print_last_error(_T("GetModuleHandle"));
-		goto Cleanup;
+		/* If lpBaseAddress initialized then hProcess is initialized too because of upper check. */
+		if (lpBaseAddress)
+		{
+			VirtualFreeEx(hProcess, lpBaseAddress, 0, MEM_RELEASE);
+		}
+		if (hProcess) CloseHandle(hProcess);
 	}
 
 	/* Get LoadLibrary address */
 	_tprintf(_T("\t[+] Looking for LoadLibrary in kernel32\n"));
-	LoadLibraryAddress = GetProcAddress(hKernel32, "LoadLibraryW");
-	if (LoadLibraryAddress == nullptr) {
+	const FARPROC LoadLibraryAddress = GetProcAddress(hKernel32, "LoadLibraryW");
+	if (LoadLibraryAddress == nullptr)
+	{
 		print_last_error(_T("GetProcAddress"));
-		goto Cleanup;
+		/* If lpBaseAddress initialized then hProcess is initialized too because of upper check. */
+		if (lpBaseAddress)
+		{
+			VirtualFreeEx(hProcess, lpBaseAddress, 0, MEM_RELEASE);
+		}
+		if (hProcess) CloseHandle(hProcess);
 	}
 	_tprintf(_T("\t[+] Found at 0x%p\n"), LoadLibraryAddress);
 
@@ -55,43 +67,51 @@ BOOL CreateRemoteThread_Injection()
 	_tprintf(_T("\t[+] Full DLL Path: %s\n"), lpDllPath);
 
 	/* Calculate the number of bytes needed for the DLL's pathname */
-	dwSize = _tcslen(lpDllPath) * sizeof(TCHAR);
+	SIZE_T dwSize = _tcslen(lpDllPath) * sizeof(TCHAR);
 
 	/* Allocate memory into the remote process */
 	_tprintf(_T("\t[+] Allocating space for the path of the DLL\n"));
 	lpBaseAddress = VirtualAllocEx(hProcess, nullptr, dwSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	if (lpBaseAddress == nullptr) {       
+	if (lpBaseAddress == nullptr)
+	{
 		print_last_error(_T("VirtualAllocEx"));
-		goto Cleanup;
+		/* If lpBaseAddress initialized then hProcess is initialized too because of upper check. */
+		if (lpBaseAddress)
+		{
+			VirtualFreeEx(hProcess, lpBaseAddress, 0, MEM_RELEASE);
+		}
+		if (hProcess) CloseHandle(hProcess);
 	}
 
 	/* Write to the remote process */
 	printf("\t[+] Writing into the current process space at 0x%p\n", lpBaseAddress);
-	if (!WriteProcessMemory(hProcess, lpBaseAddress, lpDllPath, dwSize, nullptr)) {
+	if (!WriteProcessMemory(hProcess, lpBaseAddress, lpDllPath, dwSize, nullptr))
+	{
 		print_last_error(_T("WriteProcessMemory"));
-		goto Cleanup;
+		/* If lpBaseAddress initialized then hProcess is initialized too because of upper check. */
+		if (lpBaseAddress)
+		{
+			VirtualFreeEx(hProcess, lpBaseAddress, 0, MEM_RELEASE);
+		}
+		if (hProcess) CloseHandle(hProcess);
 	}
 
 	/* Create the more thread */
-	hRemoteThread = CreateRemoteThread(hProcess, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryAddress, lpBaseAddress, NULL, nullptr);
-	if (hRemoteThread == nullptr) {
+	const HANDLE hRemoteThread = CreateRemoteThread(hProcess, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryAddress,
+	                                                lpBaseAddress, NULL, nullptr);
+	if (hRemoteThread == nullptr)
+	{
 		print_last_error(_T("CreateRemoteThread"));
 	}
-	else {
+	else
+	{
 		_tprintf(_T("Remote thread has been created successfully ...\n"));
 		WaitForSingleObject(hRemoteThread, INFINITE);
 		CloseHandle(hRemoteThread);
-		
+
 		/* assign function success return result */
 		bStatus = TRUE;
 	}
-
-Cleanup:
-	/* If lpBaseAddress initialized then hProcess is initialized too because of upper check. */
-	if (lpBaseAddress) {
-		VirtualFreeEx(hProcess, lpBaseAddress, 0, MEM_RELEASE);
-	}
-	if (hProcess) CloseHandle(hProcess);
 
 	return bStatus;
 }
